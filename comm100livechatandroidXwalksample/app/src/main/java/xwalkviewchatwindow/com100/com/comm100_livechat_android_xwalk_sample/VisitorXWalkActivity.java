f96e20e78534d5b1a8ea2f7649a5e4a6fb4e2492
Jovi <Jovi@comm100.com>
2018-02-25 17:02:44 +0800

    解决小米手机分辨率问题

--- comm100livechatandroidXwalksample/app/src/main/java/xwalkviewchatwindow/com100/com/comm100_livechat_android_xwalk_sample/VisitorXWalkActivity.java
+++ comm100livechatandroidXwalksample/app/src/main/java/xwalkviewchatwindow/com100/com/comm100_livechat_android_xwalk_sample/VisitorXWalkActivity.java
@@ -7,8 +7,10 @@ package xwalkviewchatwindow.com100.com.comm100_livechat_android_xwalk_sample;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Environment;
 import android.view.KeyEvent;
 import android.webkit.ValueCallback;
 import  android.util.Log;
@@ -26,6 +28,12 @@ import org.xwalk.core.XWalkWebResourceResponse;
 import org.xwalk.core.JavascriptInterface;
 import  android.net.http.SslError;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Properties;
+
 @SuppressWarnings("deprecation")
 @SuppressLint("SetJavaScriptEnabled")
 public class VisitorXWalkActivity extends Activity{
@@ -36,6 +44,75 @@ public class VisitorXWalkActivity extends Activity{
     private String _chatServer = "https://chatserver.comm100.com";
     private  boolean _debug=false;
     private XWalkView mXWalkView;
+
+    private int _myWidth=640;
+    private int _myHeight=480;
+    private int  _myType=0;
+    //常量
+    public static final String SYS_EMUI = "sys_emui";
+    public static final String SYS_MIUI = "sys_miui";
+    public static final String SYS_FLYME = "sys_flyme";
+    private static final String KEY_MIUI_VERSION_CODE = "ro.miui.ui.version.code";
+    private static final String KEY_MIUI_VERSION_NAME = "ro.miui.ui.version.name";
+    private static final String KEY_MIUI_INTERNAL_STORAGE = "ro.miui.internal.storage";
+    private static final String KEY_EMUI_API_LEVEL = "ro.build.hw_emui_api_level";
+    private static final String KEY_EMUI_VERSION = "ro.build.version.emui";
+    private static final String KEY_EMUI_CONFIG_HW_SYS_VERSION = "ro.confg.hw_systemversion";
+
+    public static String getSystem(){
+        String SYS="";
+        try {
+            Properties prop= new Properties();
+            prop.load(new FileInputStream(new File(Environment.getRootDirectory(), "build.prop")));
+            if(prop.getProperty(KEY_MIUI_VERSION_CODE, null) != null
+                    || prop.getProperty(KEY_MIUI_VERSION_NAME, null) != null
+                    || prop.getProperty(KEY_MIUI_INTERNAL_STORAGE, null) != null){
+                SYS = SYS_MIUI;//小米
+            }else if(prop.getProperty(KEY_EMUI_API_LEVEL, null) != null
+                    ||prop.getProperty(KEY_EMUI_VERSION, null) != null
+                    ||prop.getProperty(KEY_EMUI_CONFIG_HW_SYS_VERSION, null) != null){
+                SYS = SYS_EMUI;//华为
+            }else if(getMeizuFlymeOSFlag().toLowerCase().contains("flyme")){
+                SYS = SYS_FLYME;//魅族
+            };
+        } catch (IOException e){
+            e.printStackTrace();
+            return SYS;
+        }
+        return SYS;
+    }
+
+    public static String getMeizuFlymeOSFlag() {
+        return getSystemProperty("ro.build.display.id", "");
+    }
+
+    private static String getSystemProperty(String key, String defaultValue) {
+        try {
+            Class<?> clz = Class.forName("android.os.SystemProperties");
+            Method get = clz.getMethod("get", String.class, String.class);
+            return (String)get.invoke(clz, key, defaultValue);
+        } catch (Exception e) {
+        }
+        return defaultValue;
+    }
+    //js接口
+    public class JsInterface {
+        public JsInterface() {
+        }
+// width:{exact:640},height:{exact:480}
+        @JavascriptInterface
+        public int getCrossWidth() {
+            return _myWidth;
+        }
+        @JavascriptInterface
+        public int getCrossHeight() {
+            return _myHeight;
+        }
+        @JavascriptInterface
+        public int getMobleType() {
+            return _myType;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -182,8 +259,24 @@ public class VisitorXWalkActivity extends Activity{
         }, 500);*/
 
         // load page
-        mXWalkView.load(url,null);
 
+            //小米手机
+            if(getSystem()==SYS_MIUI)
+            {
+               /* if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M)
+                {*/
+                _myType=1;
+                _myWidth=640;
+                _myHeight=480;
+               /* }*/
+            }
+            else
+            {
+                _myType=0;
+            }
+        //注入js
+        mXWalkView.addJavascriptInterface(new JsInterface(), "NativeInterface");
+        mXWalkView.load(url,null);
         //https://webrtc.github.io/samples/src/content/getusermedia/gum/
         //resolution
         // https://www.webrtc-experiment.com/websocket/
